ee74da1685465f9bd0701aeb2fe3b51bc12e712d
Josh Medeski <josh.medeski@gmail.com>
2023-06-27 10:32:48 -0500

    feat: add improved code snippet
    Taken from astro docs repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,23 @@
+import {
+  astroCodeSnippets,
+  codeSnippetAutoImport,
+} from "./integrations/astro-code-snippets";
 import mdx from "@astrojs/mdx";
 import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import AutoImport from "astro-auto-import";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://joshmedeski.com/",
-  integrations: [mdx(), sitemap(), tailwind(), preact()],
+  integrations: [
+    AutoImport({ imports: [codeSnippetAutoImport] }),
+    astroCodeSnippets(),
+    mdx(),
+    sitemap(),
+    tailwind(),
+    preact(),
+  ],
 });
