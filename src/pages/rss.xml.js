1c9c8e0982e81c0b5f7d23fa240fda7475d37b7f
Josh Medeski <josh.medeski@gmail.com>
2023-08-30 20:35:15 -0500

    fix: capitalization of rss GET function

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,11 @@
 import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 import rss from "@astrojs/rss";
 
-export const get = () =>
-  rss({
+export function GET() {
+  return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: import.meta.env.SITE,
     items: import.meta.glob("./posts/*.mdx"),
   });
+}
