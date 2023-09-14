7eb33997f37d9425a5d5ea59c38a7bf762a848f0
Josh Medeski <josh.medeski@gmail.com>
2023-09-13 21:00:23 -0500

    fix: use frozen-lockfile

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ export BUN_INSTALL=$HOME/.bun
 export PATH=$BUN_INSTALL/bin:$PATH
 
 bun --version
-bun install
+bun install --frozen-lockfile
 prisma migrate dev
 cat /opt/build/repo/node_modules/load-yaml-file/node_modules/js-yaml/package.json
 bun --bun run astro build
