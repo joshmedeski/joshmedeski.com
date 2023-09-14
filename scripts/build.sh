737fdd7aa283a2f4ae20dae0933e66000239e33e
Josh Medeski <josh.medeski@gmail.com>
2023-09-13 21:14:15 -0500

    fix: run prisma migrate dev on build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,4 @@ export PATH=$BUN_INSTALL/bin:$PATH
 
 bun --version
 bun install --verbose --frozen-lockfile
-prisma migrate dev
-ls /opt/build/repo/node_modules/load-yaml-file
 bun --bun run astro build
