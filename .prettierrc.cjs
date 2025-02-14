90c545f6fade53727a7f59f123446e7a839472eb
Josh Medeski <josh.medeski@gmail.com>
2025-02-13 18:34:52 -0600

    feat: add biome

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: [
-    require.resolve("@trivago/prettier-plugin-sort-imports"),
-    require.resolve("prettier-plugin-astro"),
-    require.resolve("prettier-plugin-tailwindcss"),
+    require.resolve('@trivago/prettier-plugin-sort-imports'),
+    require.resolve('prettier-plugin-astro'),
+    require.resolve('prettier-plugin-tailwindcss'),
   ],
   pluginSearchDirs: false,
-};
+}
