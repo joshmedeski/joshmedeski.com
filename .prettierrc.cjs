f17d499a56111714f78f684ffb33265f000e1bd0
Josh Medeski <josh.medeski@gmail.com>
2025-02-13 18:42:38 -0600

    chore: remove prettier eslint config files

--- .prettierrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    require.resolve('@trivago/prettier-plugin-sort-imports'),
-    require.resolve('prettier-plugin-astro'),
-    require.resolve('prettier-plugin-tailwindcss'),
-  ],
-  pluginSearchDirs: false,
-}
