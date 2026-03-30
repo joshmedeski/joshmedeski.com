6bfe8e7bb9d92f050586a3df77931b173283fe5b
Josh Medeski <josh.medeski@gmail.com>
2026-03-30 11:14:10 -0500

    Style callouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 import expressiveCode from 'astro-expressive-code'
 import pagefind from 'astro-pagefind'
 import { defineConfig } from 'astro/config'
+import remarkGithubAlerts from 'remark-github-alerts'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://joshmedeski.com/',
   prefetch: true,
+
+  markdown: {
+    remarkPlugins: [remarkGithubAlerts],
+  },
 
   integrations: [
     // NOTE: expressiveCode must be before mdx
